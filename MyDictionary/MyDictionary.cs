f93a00e3a4a846937cc831e6ef007ccb4bfcd094
rakif34 <62598898+rakif34@users.noreply.github.com>
2021-03-14 17:26:37 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T,F>
+    {
+
+        T[] names;
+        F[] ages;
+
+        //constructor
+
+        public MyDictionary()
+        {
+            names = new T[0];
+            ages = new F[0];
+        }
+
+        public void Add(T name, F age)
+        {
+            T[] tempArrayN = names;
+            names = new T[names.Length + 1];
+            for (int i = 0; i < tempArrayN.Length; i++)
+            {
+                names[i] = tempArrayN[i];
+            }
+            names[names.Length - 1] = name;
+
+            F[] tempArrayA = ages;
+            ages = new F[ages.Length + 1];
+            for (int i = 0; i < tempArrayA.Length; i++)
+            {
+                ages[i] = tempArrayA[i];
+            }
+            ages[ages.Length - 1] = age;
+
+
+
+        }
+
+        public void List()
+        {
+            for (int j = 0; j < names.Length; j++)
+            {
+                Console.WriteLine("Name: " + names[j] +  " Age :" + ages[j] );
+            }
+        }
+
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+            MyDictionary<string, int> adYas = new MyDictionary<string, int>();
+
+
+            adYas.Add("Ahmet", 30);
+            adYas.Add("Mehmet", 40);
+            adYas.Add("Veli", 25);
+            adYas.Add("Banu", 23);
+            adYas.Add("Sedat", 22);
+
+            adYas.List();
+
+
+        }
+    }
+}
